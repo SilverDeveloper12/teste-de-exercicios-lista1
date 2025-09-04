4dc5e4a9697133b6d0d090018f0820573d006ed0
SilverDeveloper12 <167832190+SilverDeveloper12@users.noreply.github.com>
2025-09-04 18:45:42 -0300

    Add files via upload

--- Teste exercicios pica em Java de algorithms mermao ala/src/teste/exercicios/pica/em/java/de/algorithms/mermao/ala/Exerciciodouze.java
+++ Teste exercicios pica em Java de algorithms mermao ala/src/teste/exercicios/pica/em/java/de/algorithms/mermao/ala/Exerciciodouze.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package teste.exercicios.pica.em.java.de.algorithms.mermao.ala;
+import java.util.Scanner;
 
-/**
- *
- * @author enzo.ocsilva
- */
+
 public class Exerciciodouze {
+    public static void main(String []args){
+    Scanner input = new Scanner(System.in);
+    double L,R,pi,calcL,calcR;
     
+    System.out.print("Digite o valor do lado do quadrado: ");
+    L = input.nextDouble();
+    System.out.print("Digite agora o valor do raio do circulo: ");
+    R = input.nextDouble();
+    
+    pi = Math.PI;
+    calcL = (L * L) * 2;
+    calcR = pi * (R * R);
+    
+        System.out.println(calcL);
+        
+    if(calcL > calcR){
+        System.out.println("A area do quadrado e maior!");
+    }else{
+        System.out.println("A area da circunferencia e maior!");
+        }
+    }
 }
